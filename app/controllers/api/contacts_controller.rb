d9b0196150c5a5bb6bb505eba57d273d9ff39557
brian <riceb53@gmail.com>
2020-07-06 20:15:35 -0400

    remove non RESTful stuff

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,2 @@
 class Api::ContactsController < ApplicationController
-  def the_contact
-    @contact = Contact.second
-    render 'contact.json.jb'
-  end
-
-  def every_contact
-    @contacts = Contact.all    
-    render 'the_contacts.json.jb'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-          #  url            controller   method
-    get "/first_contact" => "contacts#the_contact"
-
-    get "/all_contacts" => "contacts#every_contact"
   end
 end
